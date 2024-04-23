fcda2827d3797aef8ac1f5bd4d20a27a95a637d2
JhonJMD <152086681+JhonJMD@users.noreply.github.com>
2024-04-23 10:44:30 -0500

    Excersice 2 done

--- Ejercicio02/js/js02.js
+++ Ejercicio02/js/js02.js
@@ -2,6 +2,17 @@
 Escribir una función llamada calcularImpuestos que reciba dos argumentos numéricos: edad e ingresos. Si
 edad es igual o mayor a 18 y los ingresos son iguales o mayores a 1000 debe retornar ingresos * 40%. De lo
 contrario retornar 0.*/
+function calcularImpuestos(edad, ingresos){
+    if (edad >= 18){
+        if (ingresos >= 1000){
+            return ingresos*0.40;
+        }else{
+            return 0;
+        }
+    }else{
+        return 0;
+    }
+}
 function validacion(mensaje){
     while (true){
         let input = prompt(mensaje);
@@ -18,16 +29,7 @@ function validacion(mensaje){
 }
 let mensajeInput = "Ingrese la Edad"
 let edadUser = validacion(mensajeInput);
+mensajeInput = "Ingrese los ingresos";
+let ingresosUser = validacion(mensajeInput);
+alert("El porcentaje de impuestos es: "+calcularImpuestos(edadUser,ingresosUser))
 
-
-function calcularImpuestos(edad, ingresos){
-    if (edad >= 18){
-        if (ingresos >= 1000){
-            return ingresos*0.40;
-        }else{
-            return 0;
-        }
-    }else{
-        return 0;
-    }
-}
